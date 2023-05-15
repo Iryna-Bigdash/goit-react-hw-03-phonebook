d523bd9411cff7d3f8636eaf7791ce9736efa73c
Iryna B <imbigdash@gmail.com>
2023-05-15 14:58:24 +0100

    added logic for Local Storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,14 @@ import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import { nanoid } from 'nanoid';
 import Filter from '../Filter/Filter';
-import {Container, Title} from './App.styled';
+import { Container, Title } from './App.styled';
 export default class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -48,24 +48,38 @@ export default class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
 
     return (
       <>
-      <Container>
-        <Title>Phonebook</Title>
-        <ContactForm onSubmit={this.formSubmitHandler} />
+        <Container>
+          <Title>Phonebook</Title>
+          <ContactForm onSubmit={this.formSubmitHandler} />
 
-        <Title>Contacts</Title>
-        <Filter value={filter} onChange={this.changeFilter} />
+          <Title>Contacts</Title>
+          <Filter value={filter} onChange={this.changeFilter} />
 
-        <ContactList
-          contacts={visibleContacts}
-          onDeletContact={this.deleteContact}
-        />
-      </Container>
+          <ContactList
+            contacts={visibleContacts}
+            onDeletContact={this.deleteContact}
+          />
+        </Container>
       </>
     );
   }
